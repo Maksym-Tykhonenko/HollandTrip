b7700deb6e525b5cf77ed442264512d3fb208bc3
Maksym-Tykhonenko <97916981+Maksym-Tykhonenko@users.noreply.github.com>
2024-03-22 11:25:08 +0200

    initial commit

---
+++ hollandTrip/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Hello World!!!!</Text>
+      <Text>It's a HollandTrip app!</Text>
+    </View>
+  );
+};
+
+export default App;
