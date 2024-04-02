e4e2ce716ef9b379fd63df0d9311f0dd27539a8e
Maksym-Tykhonenko <97916981+Maksym-Tykhonenko@users.noreply.github.com>
2024-04-02 12:42:43 +0300

    1.0.0(1)

--- hollandTrip/screens/ProfileScreen.js
+++ hollandTrip/screens/ProfileScreen.js
@@ -8,10 +8,16 @@ import {
   TextInput,
   TouchableOpacity,
   View,
+  Modal,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from 'react-native';
+import {Calendar, LocaleConfig} from 'react-native-calendars';
 import {Dimensions} from 'react-native';
 import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {uid} from 'uid';
 
 const ProfileScreen = () => {
   const windowWidth = Dimensions.get('window').width;
@@ -31,9 +37,19 @@ const ProfileScreen = () => {
   const [visited10, setVisited10] = useState(false);
   //////////////
   const [onChangeName, setOnChangeName] = useState('');
-  console.log('onChangeName==>', onChangeName);
+  //console.log('onChangeName==>', onChangeName);
   const [name, setName] = useState();
-  console.log('name==>', name);
+  //console.log('name==>', name);
+  //////////////
+  const [rollUpBlock, setRollUpBlock] = useState(false);
+  const [modalClose, setModalClose] = useState(false);
+
+  const [selected, setSelected] = useState('');
+  const [place, setPlace] = useState('');
+  const [fidback, setFidback] = useState('');
+  //console.log('selected', selected);
+  const [eventPlace, setEventPlace] = useState([]);
+  console.log('eventPlace', eventPlace);
 
   useEffect(() => {
     getData();
@@ -54,6 +70,7 @@ const ProfileScreen = () => {
     visited7,
     visited8,
     visited10,
+    eventPlace,
   ]);
   //, selectPhoto
   const setData = async () => {
@@ -71,12 +88,13 @@ const ProfileScreen = () => {
         visited7,
         visited8,
         visited10,
+        eventPlace,
       };
       //const dataPhoto = [selectPhoto];
       const jsonData = JSON.stringify(data);
       await AsyncStorage.setItem(`ProfileScreen`, jsonData);
       console.log('Дані збережено в AsyncStorage');
-      console.log('55', jsonData);
+      //console.log('55', jsonData);
     } catch (e) {
       console.log('Помилка збереження даних:', e);
     }
@@ -100,7 +118,7 @@ const ProfileScreen = () => {
         setVisited7(parsedData.visited7);
         setVisited8(parsedData.visited8);
         setVisited10(parsedData.visited10);
-
+        setEventPlace(parsedData.eventPlace);
         //setSelectPhoto(parsedData.selectPhoto);
       }
     } catch (e) {
@@ -145,6 +163,26 @@ const ProfileScreen = () => {
         console.log('Вибір скасовано');
       }
     });
+  };
+
+  const handleAddDataAboutEventPlace = () => {
+    let newEventPlace = {
+      place,
+      fidback,
+      selected,
+    };
+    //console.log('newFishingPlace', newFishingPlace);
+
+    setEventPlace([newEventPlace, ...eventPlace]);
+
+    modalPopUpClose();
+  };
+
+  const modalPopUpClose = () => {
+    setModalClose(false);
+    setPlace('');
+    setFidback('');
+    setSelected('');
   };
 
   return (
@@ -296,8 +334,8 @@ const ProfileScreen = () => {
                 </Text>
               </View>
             )}
+            {/**INTERACTIVE MAP BLOCK position: 'absolute'*/}
 
-            {/**INTERACTIVE MAP BLOCK */}
             <View
               style={{
                 alignItems: 'center',
@@ -477,7 +515,7 @@ const ProfileScreen = () => {
                   <Text style={{fontSize: 18, fontWeight: 'bold'}}>8</Text>
                 </TouchableOpacity>
 
-                {/**10 BTN */}
+                {/**9 BTN */}
                 <TouchableOpacity
                   onPress={() => setVisited10(true)}
                   style={{
@@ -492,12 +530,17 @@ const ProfileScreen = () => {
                     alignItems: 'center',
                     justifyContent: 'center',
                   }}>
-                  <Text style={{fontSize: 18, fontWeight: 'bold'}}>10</Text>
+                  <Text style={{fontSize: 18, fontWeight: 'bold'}}>9</Text>
                 </TouchableOpacity>
               </View>
             </View>
             {/**List of places */}
-            <View style={{alignItems: 'center', marginTop: 5}}>
+            <View
+              style={{
+                alignItems: 'center',
+                marginTop: 5,
+                borderBottomWidth: 3,
+              }}>
               <View
                 style={{
                   marginBottom: 10,
@@ -509,46 +552,257 @@ const ProfileScreen = () => {
 
                   backgroundColor: '#0db561',
                 }}>
-                <View style={{alignItems: 'center', justifyContent: 'center'}}>
-                  <Text style={{fontFamily: 'Chewy-Regular', fontSize: 20}}>
+                <View
+                  style={{
+                    position: 'relative',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                  }}>
+                  <TouchableOpacity
+                    onPress={() => {
+                      setRollUpBlock(!rollUpBlock);
+                    }}
+                    style={{position: 'absolute', right: 10}}>
+                    {rollUpBlock ? (
+                      <Image
+                        source={require('../assets/png/free-icon-arrow-14025285.png')}
+                        style={{
+                          width: 35,
+                          height: 35,
+                          borderRadius: 15,
+                          borderWidth: 3,
+                        }}
+                      />
+                    ) : (
+                      <Image
+                        source={require('../assets/png/free-icon-arrow-14025545.png')}
+                        style={{
+                          width: 35,
+                          height: 35,
+                          borderRadius: 15,
+                          borderWidth: 3,
+                        }}
+                      />
+                    )}
+                  </TouchableOpacity>
+
+                  <Text style={{fontFamily: 'Chewy-Regular', fontSize: 25}}>
                     List of places:
                   </Text>
                 </View>
 
-                <Text style={{fontFamily: 'Chewy-Regular', fontSize: 20}}>
-                  - 1.Rijksmuseum. Museumstraat 1, 1071 XX Amsterdam
-                </Text>
-                <Text style={{fontFamily: 'Chewy-Regular', fontSize: 20}}>
-                  - 2.Anne Frank House. Prinsengracht 263-267, 1016 GV Amsterdam
-                </Text>
-                <Text style={{fontFamily: 'Chewy-Regular', fontSize: 20}}>
-                  - 3.Keukenhof Gardens. Stationsweg 166A, 2161 AM Lisse
-                </Text>
-                <Text style={{fontFamily: 'Chewy-Regular', fontSize: 20}}>
-                  - 4.Van Gogh Museum. Museumplein 6, 1071 DJ Amsterdam
-                </Text>
-                <Text style={{fontFamily: 'Chewy-Regular', fontSize: 20}}>
-                  - 5.Zaanse Schans. Schansend 7, 1509 AW Zaandam
-                </Text>
-                <Text style={{fontFamily: 'Chewy-Regular', fontSize: 20}}>
-                  - 6.A'DAM Lookout. Overhoeksplein 5, 1031 KS Amsterdam
-                </Text>
-                <Text style={{fontFamily: 'Chewy-Regular', fontSize: 20}}>
-                  - 7.Kinderdijk. Nederwaard 1, 2961 AS Kinderdijk
-                </Text>
-                <Text style={{fontFamily: 'Chewy-Regular', fontSize: 20}}>
-                  - 8.Efteling Theme Park Resort. Europalaan 1, 5171 KW
-                  Kaatsheuvel
-                </Text>
-                <Text style={{fontFamily: 'Chewy-Regular', fontSize: 20}}>
-                  - 10.Hotel Pulitzer Amsterdam. Prinsengracht 315-331, 1016 GZ
-                  Amsterdam
-                </Text>
+                {!rollUpBlock && (
+                  <View>
+                    <Text style={{fontFamily: 'Chewy-Regular', fontSize: 20}}>
+                      - 1.Rijksmuseum. Museumstraat 1, 1071 XX Amsterdam
+                    </Text>
+                    <Text style={{fontFamily: 'Chewy-Regular', fontSize: 20}}>
+                      - 2.Anne Frank House. Prinsengracht 263-267, 1016 GV
+                      Amsterdam
+                    </Text>
+                    <Text style={{fontFamily: 'Chewy-Regular', fontSize: 20}}>
+                      - 3.Keukenhof Gardens. Stationsweg 166A, 2161 AM Lisse
+                    </Text>
+                    <Text style={{fontFamily: 'Chewy-Regular', fontSize: 20}}>
+                      - 4.Van Gogh Museum. Museumplein 6, 1071 DJ Amsterdam
+                    </Text>
+                    <Text style={{fontFamily: 'Chewy-Regular', fontSize: 20}}>
+                      - 5.Zaanse Schans. Schansend 7, 1509 AW Zaandam
+                    </Text>
+                    <Text style={{fontFamily: 'Chewy-Regular', fontSize: 20}}>
+                      - 6.A'DAM Lookout. Overhoeksplein 5, 1031 KS Amsterdam
+                    </Text>
+                    <Text style={{fontFamily: 'Chewy-Regular', fontSize: 20}}>
+                      - 7.Kinderdijk. Nederwaard 1, 2961 AS Kinderdijk
+                    </Text>
+                    <Text style={{fontFamily: 'Chewy-Regular', fontSize: 20}}>
+                      - 8.Efteling Theme Park Resort. Europalaan 1, 5171 KW
+                      Kaatsheuvel
+                    </Text>
+                    <Text style={{fontFamily: 'Chewy-Regular', fontSize: 20}}>
+                      - 9.Hotel Pulitzer Amsterdam. Prinsengracht 315-331, 1016
+                      GZ Amsterdam
+                    </Text>
+                  </View>
+                )}
               </View>
+            </View>
+
+            {/**calendar block */}
+            <View
+              style={{alignItems: 'center', marginTop: 15, marginBottom: 15}}>
+              <TouchableOpacity
+                onPress={() => {
+                  setModalClose(true);
+                  //console.log('hello');
+                }}
+                style={{
+                  borderWidth: 3,
+                  borderRadius: 15,
+                  backgroundColor: '#0db561',
+                  height: 50,
+                  width: 300,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}>
+                <Text style={{fontFamily: 'Chewy-Regular', fontSize: 25}}>
+                  Add your event
+                </Text>
+              </TouchableOpacity>
+            </View>
+
+            <View>
+              {eventPlace.map(i => {
+                return (
+                  <View
+                    style={{
+                      width: windowWidth * 0.9,
+                      marginBottom: 10,
+                      padding: 10,
+                      borderWidth: 3,
+                      borderRadius: 15,
+                      backgroundColor: '#0db561',
+                    }}
+                    key={uid()}>
+                    <Text>{i.selected}</Text>
+                    <Text style={{fontFamily: 'Chewy-Regular', fontSize: 18}}>
+                      {i.place}
+                    </Text>
+                    <Text style={{fontFamily: 'Chewy-Regular', fontSize: 18}}>
+                      {i.fidback}
+                    </Text>
+                  </View>
+                );
+              })}
             </View>
 
             {/**LOWER INDEN */}
             <View style={{height: 100}}></View>
+
+            <Modal animationType="fade" transparent={true} visible={modalClose}>
+              <View
+                style={{
+                  alignItems: 'center',
+                  paddingTop: 40,
+                  backgroundColor: '#0db561',
+                  flex: 1,
+                  marginRight: '5%',
+                  marginLeft: '5%',
+                  marginTop: '10%',
+                  marginBottom: '10%',
+                  borderRadius: 15,
+                  borderWidth: 3,
+                  borderColor: '#000',
+                }}>
+                {/**BTN SideBar Close */}
+                <TouchableOpacity
+                  onPress={() => {
+                    modalPopUpClose();
+                  }}
+                  style={{position: 'absolute', top: 20, left: 10}}>
+                  <Text style={{color: '#000', fontSize: 30}}>X</Text>
+                </TouchableOpacity>
+
+                <KeyboardAvoidingView
+                  style={{flex: 1}}
+                  behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+                  <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <ScrollView style={{marginBottom: 50}}>
+                      <View>
+                        {/** */}
+                        <Calendar
+                          onDayPress={day => {
+                            setSelected(day.dateString);
+                          }}
+                          markedDates={{
+                            [selected]: {
+                              selected: true,
+                              disableTouchEvent: true,
+                              selectedDotColor: 'orange',
+                            },
+                          }}
+                        />
+
+                        <TextInput
+                          placeholderTextColor="rgba(0, 0, 0, 0.5)"
+                          placeholder="Event place..."
+                          value={place}
+                          onChangeText={setPlace}
+                          style={{
+                            shadowOffset: {width: 3, height: 4},
+                            shadowOpacity: 0.8,
+                            elevation: 9,
+                            marginTop: 20,
+                            marginBottom: 15,
+                            paddingLeft: 10,
+                            fontSize: 20,
+                            borderWidth: 3,
+                            borderColor: '#000',
+                            color: '#000',
+                            backgroundColor: 'rgba(255, 255, 255, 0.5)',
+                            borderRadius: 10,
+                            width: 250,
+                            height: 40,
+                          }}
+                        />
+
+                        <TextInput
+                          placeholderTextColor="rgba(0, 0, 0, 0.5)"
+                          placeholder="My feedback..."
+                          multiline={true}
+                          value={fidback}
+                          onChangeText={setFidback}
+                          style={{
+                            shadowOffset: {width: 3, height: 4},
+                            shadowOpacity: 0.8,
+                            elevation: 9,
+                            marginTop: 5,
+                            marginBottom: 15,
+                            paddingLeft: 10,
+                            fontSize: 20,
+                            borderWidth: 3,
+                            borderColor: '#000',
+                            color: '#000',
+                            backgroundColor: 'rgba(255, 255, 255, 0.5)',
+                            borderRadius: 10,
+                            width: 250,
+                            height: 80,
+                          }}
+                        />
+
+                        <View style={{alignItems: 'center'}}>
+                          <TouchableOpacity
+                            onPress={() => {
+                              handleAddDataAboutEventPlace();
+                            }}
+                            style={{
+                              shadowOffset: {width: 3, height: 4},
+                              shadowOpacity: 0.8,
+                              elevation: 9,
+                              marginTop: 20,
+                              marginBottom: 15,
+                              paddingLeft: 10,
+                              fontSize: 20,
+                              borderWidth: 3,
+                              borderColor: '#000',
+                              color: '#000',
+                              backgroundColor: 'rgba(255, 255, 255, 0.5)',
+                              borderRadius: 15,
+                              width: 100,
+                              height: 40,
+                            }}>
+                            <Text style={{color: '#000', fontSize: 30}}>
+                              SAVE
+                            </Text>
+                          </TouchableOpacity>
+                        </View>
+                      </View>
+                      <View style={{height: 150}}></View>
+                    </ScrollView>
+                  </TouchableWithoutFeedback>
+                </KeyboardAvoidingView>
+              </View>
+            </Modal>
           </ScrollView>
         </SafeAreaView>
       </ImageBackground>
